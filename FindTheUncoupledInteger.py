bc5f065c080599236d1bc07fff3fa1e6e2a2674d
Haruto K <aoi.stella.dev@gmail.com>
2025-04-27 21:32:16 +0900

    not solve FIndTheUncoupledInteger. commit for storin codes

--- FindTheUncoupledInteger.py
+++ FindTheUncoupledInteger.py
@@ -1,5 +1,10 @@
-nums = list(map(int, input().split(",")))
-ans = nums[0]
-for i in range(1, len(nums)):
-    ans = ans ^ nums[i]
-print(ans)
+def getWays(n, coins):
+    # dp[i] = 金額iを作る方法の数
+    dp = [0] * (n + 1)
+    dp[0] = 1  # 基本ケース: 0円を作る方法は1通り(何も選ばない)
+    
+    for coin in coins:
+        for i in range(coin, n + 1):
+            dp[i] += dp[i - coin]
+    
+    return dp[n]
